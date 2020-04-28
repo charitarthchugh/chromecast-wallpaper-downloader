59486a26ad24c238b9e6af537f223c106860a8e5
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-04-28 09:55:19 -0400

    Update readme with credits and running instructions

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,17 @@
 package main.java;
 
 
-public class main {
-    public static void main(String[] args) {
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class main {
+    public static void main(String[] args) throws FileNotFoundException {
+        JsonObject jsonObject= (JsonObject) new JsonParser().parse(new FileReader("src/main/resources/backgrounds.json"));
+        JsonArray jsonArray=jsonObject.getAsJsonArray("urls");
+        System.out.println((jsonArray==null));
     }
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,7 +1,22 @@
 package test.java;
 
-public class test {
-    public static void main(String[] args) {
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class test {
+    public static void main(String[] args) throws FileNotFoundException {
+        JsonObject jsonObject= (JsonObject) new JsonParser().parse(new FileReader("src/test/resources/test.json"));
+        JsonArray jsonArray=jsonObject.getAsJsonArray("urls");
+        System.out.println((jsonArray==null));
+        for(int i=0;i<jsonArray.size();i++){
+
+        }
     }
 }
