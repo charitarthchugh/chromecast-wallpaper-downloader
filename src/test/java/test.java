89d21166d00dc94a17c1953c561b124be36049c9
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-04-28 18:16:22 -0400

    Get test to work!!!
    Eventing works as intended in test.java using test.json
    Had to import Apache commons to use fileutils
    Had to fix Maven pom-change compiler
    add images directory- where images will be downloaded- WILL NOT BE TRACKED
    plus many other small changes

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,22 +1,29 @@
-package test.java;
-
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
+import java.net.URL;
 
 public class test {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
         JsonObject jsonObject= (JsonObject) new JsonParser().parse(new FileReader("src/test/resources/test.json"));
         JsonArray jsonArray=jsonObject.getAsJsonArray("urls");
-        System.out.println((jsonArray==null));
+        Assert.assertNotNull(jsonArray);
+        String url, fileName;
         for(int i=0;i<jsonArray.size();i++){
-
+            url=jsonArray.get(i).getAsJsonObject().get("url").getAsString();
+            fileName="images/"+url.substring(url.lastIndexOf("/")+1);
+            System.out.println(fileName);
+            try {
+                FileUtils.copyURLToFile(new URL(url),new File(fileName));
+            }catch (FileNotFoundException file){
+                System.out.println("Could not download file at url: \n"+url+"\nfile:" +fileName+"\n ...Skipping file(probably file itself is not available");
+            }
         }
     }
 }
